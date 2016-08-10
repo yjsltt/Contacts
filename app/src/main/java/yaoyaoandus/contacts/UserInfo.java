a7d1d32245c6c4bd1a2326b2d9249cf4d2bbc9e8
Ryin <583480035@qq.com>
2016-08-10 21:20:32 +0800

    login登录按钮链接到main活动

---
+++ app/src/main/java/yaoyaoandus/contacts/UserInfo.java
@@ -0,0 +1,19 @@
+package yaoyaoandus.contacts;
+
+/**
+ * Created by lenovo on 2016/8/4.
+ */
+public class UserInfo {
+    public String userPhoto=null;
+    public String userNum=null;
+    public String deleteButtonRes=null;
+    public UserInfo(String userPhoto, String userNum, String deleteButtonRes
+                    ) {
+        super();
+        this.userPhoto = userPhoto;
+        this.userNum= userNum;
+        this.deleteButtonRes = deleteButtonRes;
+
+    }
+
+}
